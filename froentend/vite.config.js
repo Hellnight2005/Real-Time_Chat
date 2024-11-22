f7dec272aab36569fe70a2fade552fab233e2ab9
Abhijeet <abhijeet2005shinde@gmail.com>
2024-11-22 23:24:00 +0530

    Seven Day

--- froentend/vite.config.js
+++ froentend/vite.config.js
@@ -4,7 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: { "/api": "http://localhost:5000" },
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // Backend server URL
+        changeOrigin: true,
+        secure: false,
+      },
+      "/socket.io": {
+        target: "http://localhost:5000", // Backend URL for Socket.IO
+        ws: true, // Enable WebSocket proxying
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [react()],
 });
